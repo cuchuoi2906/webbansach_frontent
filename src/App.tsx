af65bf09befb2312f40436a20fbf666f2016c4cc
Phạm tuấn anh <p.anhft@gmail.com>
2024-04-11 16:46:35 +0700

    header  footer

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './layout/header/Navbar';
+import Footer from './layout/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
