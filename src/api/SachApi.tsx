7f1acd77616b42461ccdf84d0585ad2b861437ef
Tuan anh <p.anhft@gmail.com>
2024-05-05 21:51:50 +0700

    aaa

--- src/api/SachApi.tsx
+++ src/api/SachApi.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import SachModel from "../Model/SachModel";
 import { Request_api } from "./Request_api";
 
-export async function layToanBoSach():Promise<SachModel[]> {
-    const ketQua:SachModel[] = [];
-    const urlSach: string = "http://localhost:8080/sach";
+interface KetQuaInterface {
+    tongSoTrang: number;
+    ketQua: SachModel[];
+    tongSoSach: number;
+}
+
+async function laySach(urlSach: string): Promise<KetQuaInterface> {
+    const ketQua: SachModel[] = [];
     // Gọi phương thức request
     const response = await Request_api(urlSach);
-    const responseData =  response._embedded.saches;
+    const responseData = response._embedded.saches;
+
+    // lấy thông tin trang
+    const tongSoTrang: number = response.page.totalPages;
+    const tongSoSach: number = response.page.totalElements;
 
     for (const key in responseData) {
         ketQua.push({
@@ -15,11 +24,17 @@ export async function layToanBoSach():Promise<SachModel[]> {
             tenSach: responseData[key].tenSach,
             giaBan: responseData[key].giaBan,
             giaNiemYet: responseData[key].giaNiemYet,
-            moTa:responseData[key].moTa,
-            soLuong:responseData[key].soLuong,
-            tenTacGia:responseData[key].tenTacGia,
-            trungBinhXepHang:responseData[key].trungBinhXepHang
+            moTa: responseData[key].moTa,
+            soLuong: responseData[key].soLuong,
+            tenTacGia: responseData[key].tenTacGia,
+            trungBinhXepHang: responseData[key].trungBinhXepHang
         });
     }
-    return ketQua;
+    return { ketQua: ketQua, tongSoSach: tongSoTrang, tongSoTrang: tongSoTrang };
+}
+
+export async function layToanBoSach(trang): Promise<KetQuaInterface> {
+    // Xác định endpoint
+    const urlSach: string = `http://localhost:8080/sach?sort=maSach,desc&size=8&page=${trang}`;
+    return laySach(urlSach);
 }
--- src/layout/products/DanhSachSanPham.tsx
+++ src/layout/products/DanhSachSanPham.tsx
@@ -2,45 +2,53 @@ import React, { useEffect, useState } from "react";
 import SachProps from "./component/SachProps";
 import SachModel from "../../Model/SachModel";
 import { layToanBoSach } from "../../api/SachApi";
+import { Paginations } from "../../utils/Paginations";
 
-const DanhSachSanPham: React.FC = ()=>{
-    const [danhSachQuyenSach,setDanhSachQuyenSach] = useState<SachModel[]>([]);
-    const [dangTaiDuLieu,setDangTaiDuLieu] = useState(true);
-    const [baoLoi,setBaoLoi] = useState(null);
+const DanhSachSanPham: React.FC = () => {
+    const [danhSachQuyenSach, setDanhSachQuyenSach] = useState<SachModel[]>([]);
+    const [dangTaiDuLieu, setDangTaiDuLieu] = useState(true);
+    const [baoLoi, setBaoLoi] = useState(null);
+    const [trangHienTai, setTrangHienTai] = useState(1);
+    const [tongSoTrang, setTongSoTrang] = useState(0);
 
-    useEffect(()=>{
-        layToanBoSach().then(sachData=>{
-            setDanhSachQuyenSach(sachData);
+    useEffect(() => {
+        layToanBoSach(trangHienTai - 1).then(kp => {
+            setDanhSachQuyenSach(kp.ketQua);
             setDangTaiDuLieu(false);
+            setTongSoTrang(kp.tongSoTrang);
         }).catch(
             error => {
                 setDangTaiDuLieu(false);
                 setBaoLoi(error.message);
             }
         );
-    },[]); // [] chỉ gọi 1 lần
+    }, [trangHienTai]); // [] chỉ gọi 1 lần
 
-    if(dangTaiDuLieu){
-        return(
+    if (dangTaiDuLieu) {
+        return (
             <div>
                 <h1>Đang tải dữ liệu</h1>
             </div>
         );
     }
-    if(baoLoi){
-        return(
+    if (baoLoi) {
+        return (
             <div><h1>Gặp lỗi: {baoLoi}</h1></div>
         );
+    }
+    const phanTrang = (pageCurrent: number) => {
+        setTrangHienTai(pageCurrent);
     }
     return (
         <div className="container">
             <div className="row mt-4">
                 {
-                    danhSachQuyenSach.map((QuyenSach)=>(
+                    danhSachQuyenSach.map((QuyenSach) => (
                         <SachProps sach={QuyenSach} />
                     ))
                 }
             </div>
+            <Paginations trangHienTai={trangHienTai} tongSoTrang={tongSoTrang} phanTrang={phanTrang} />
         </div>
     );
 }
